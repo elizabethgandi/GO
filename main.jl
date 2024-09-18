aed10b1b7c73e2562aff6380eb57079e70379662
Elliot Braud <87452030+ESB24@users.noreply.github.com>
2024-09-18 14:05:28 +0200

    Update main.jl
    Bloc d'instruction pour appliquer la methode de newton.

--- main.jl
+++ main.jl
@@ -11,3 +11,16 @@ sinc_derive_seconde(x::Float64)::Float64 = return (x == 0) ? (-0.33333333315479)
 # Methode de newton
 N(x::Float64)::Float64 = return (x - (sinc_derive_premiere(x) / sinc_derive_seconde(x)))
 
+# apply the newton method starting from x
+begin
+    x   ::Float64                   = 3.        # starting point noted xk
+    tmp ::Union{Float64, Nothing}   = x-1       # xk-1
+
+    threshold ::Float64 = 0.0000000000001       # accuracy 
+    while !(tmp <= x <= tmp + threshold)
+        tmp = x
+        x = N(x)
+    end
+
+    println("x = $(x)")
+end
